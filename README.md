4a676aaddb60f4526682adc3137469afdd437193
DaiiSs <153877332+DaiiSs@users.noreply.github.com>
2024-03-29 16:44:43 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# GRT
+# Graph Reasoning Transformers for Knowledge-Aware Question Answering
 Augmenting Language Models (LMs) with structured knowledge graphs (KGs) aims to leverage structured world knowledge to enhance the capability of LMs to complete knowledge-intensive tasks. However, existing methods are unable to effectively utilize the structured knowledge in a KG due to their inability to capture the rich relational semantics of knowledge triplets. Moreover, the modality gap between natural language text and KGs has become a challenging obstacle when aligning and fusing cross-modal information. To address these challenges, we propose a novel knowledge-augmented question answering (QA) model, namely, Graph Reasoning Transformers (GRT). Different from conventional node-level methods, the GRT serves knowledge triplets as atomic knowledge and utilize a triplet-level graph encoder to capture triplet-level graph features. Furthermore, to alleviate the negative effect of the modality gap on joint reasoning, we propose a representation alignment  pretraining to align the cross-modal representations and introduce a cross-modal information fusion module with attention bias to enable cross-modal information fusion. 
-# Setup
+
+## Setup
+
+- Setup conda environment
+```
+conda create -n GRT python=3.8
+conda activate GRT
+```
+- Install packages with a setup file
+```
+bash setup.sh
+```
+- Download data
+
+You can download all the preprocessed data with the [link](https://drive.google.com/drive/folders/).
+
+
+## Pre-train
+```
+bash pre-train.sh
+```
+
+## Fine-tune
+```
+bash fine-tune.sh
+```
+
+## Acknowledgement
+This repo is built upon the QAT:
+```
+https://github.com/mlvlab/QAT
+```
---
+++ fine-tune.sh
@@ -0,0 +1,84 @@
+dt=`date '+%Y%m%d_%H%M%S'`
+
+dataset="csqa"
+model='roberta-large'
+#shift
+#shift
+args=$@
+
+
+ echo "***** hyperparameters *****"
+ echo "dataset: $dataset"
+ echo "enc_name: $model"
+ echo "batch_size: $bs"
+ echo "learning_rate: elr $elr dlr $dlr"
+ echo "edge_encoder_dim $enc_dim gsc_layer $k"
+ echo "******************************"
+
+save_dir_pref='experiments'
+logs_dir_pref='logs/csqa/'
+mkdir -p $save_dir_pref
+mkdir -p $logs_dir_pref
+
+#n_epochs=80
+
+n_epochs=30
+bs=128
+mbs=8
+ebs=4
+enc_dim=32
+
+
+#elr="1e-5"
+#dlr="1e-2"
+
+elr="6e-5"
+dlr="1e-4"
+weight_decay="1e-2"
+
+
+
+dropout="1e-1"
+dropoutf="1e-1"
+drop_ratio="0.05"
+k=2
+
+tr_dim=1024
+ffn_dim=2048
+num_heads=16
+lambda="10"
+
+ent_emb=tzw
+random_ent_emb=false
+
+###### Training ######
+for seed in 1; do
+  python3 -u pre-train --dataset $dataset \
+      --encoder $model -k $k \
+      -elr $elr -dlr $dlr -bs $bs -mbs ${mbs} -ebs ${ebs} --weight_decay ${weight_decay} --seed 0\
+      --n_epochs $n_epochs --max_epochs_before_stop 20  \
+      --train_adj data/${dataset}/graph/train.graph.adj.ori2.metapath.2.q2a.seq.pk \
+      --dev_adj data/${dataset}/graph/dev.graph.adj.ori2.metapath.2.q2a.seq.pk \
+      --test_adj  data/${dataset}/graph/test.graph.adj.ori2.metapath.2.q2a.seq.pk\
+      --train_statements  data/${dataset}/statement/train_with_triple.statement2.jsonl \
+      --dev_statements  data/${dataset}/statement/dev_with_triple.statement.jsonl \
+      --test_statements  data/${dataset}/statement/test_with_triple.statement.jsonl \
+      --max_seq_len 200     \
+      --num_relation 38    \
+      --unfreeze_epoch 5 \
+      --log_interval 10 \
+      --transformer_dim ${tr_dim} \
+      --transformer_ffn_dim ${ffn_dim} \
+      --num_heads ${num_heads} \
+      --dropouttr ${dropout} \
+      --dropoutf ${dropoutf} \
+      --lr_schedule "warmup_linear" \
+      --save_model \
+      --max_node_num 44 \
+      --inverse_relation \
+      --drop_ratio ${drop_ratio} \
+      --lambda_rpe ${lambda} \
+      --without_amp \
+      --ent_emb ${ent_emb//,/ } \
+  | tee -a $logs_dir_pref/newFT_path.${dataset}_${elr}.${dlr}.${weight_decay}.${dropout}_${tr_dim}.${ffn_dim}.${num_heads}__seed${seed}_${dt}.log.txt
+done
